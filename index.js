8cf42ea0fda676a7ef594bfc02b0a179c17dc144
nimeshm05 <nimeshmysore05@gmail.com>
2021-07-11 20:00:58 +0530

    Modified version

--- index.js
+++ index.js
@@ -1,62 +1,68 @@
 function new_func() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+  var element = document.body;
+  element.classList.toggle("dark-mode");
 }
 
 function delay(n) {
-    n = n || 2000;
-    return new Promise((done) => {
-        setTimeout(() => {
-            done();
-        }, n);
-    });
+  n = n || 2000;
+  return new Promise((done) => {
+    setTimeout(() => {
+      done();
+    }, n);
+  });
 }
 
 function pageTransition() {
-    var tl = gsap.timeline();
-    tl.to(".loading-screen", {
-        duration: 1.2,
-        width: "100%",
-        left: "0%",
-        ease: "Expo.easeInOut",
-    });
+  var tl = gsap.timeline();
+  tl.to(".loading-screen", {
+    duration: 1.2,
+    width: "100%",
+    left: "0%",
+    ease: "Expo.easeInOut",
+  });
 
-    tl.to(".loading-screen", {
-        duration: 1,
-        width: "100%",
-        left: "100%",
-        ease: "Expo.easeInOut",
-        delay: 0.3,
-    });
-    tl.set(".loading-screen", { left: "-100%" });
+  tl.to(".loading-screen", {
+    duration: 1,
+    width: "100%",
+    left: "100%",
+    ease: "Expo.easeInOut",
+    delay: 0.3,
+  });
+  tl.set(".loading-screen", { left: "-100%" });
 }
 
 function contentAnimation() {
-    var tl = gsap.timeline();
-    tl.from(".animate-this", { duration: 1, y: 30, opacity: 0, stagger: 0.4, delay: 0.2 });
+  var tl = gsap.timeline();
+  tl.from(".animate-this", {
+    duration: 1,
+    y: 30,
+    opacity: 0,
+    stagger: 0.4,
+    delay: 0.2,
+  });
 }
 
 $(function () {
-    barba.init({
-        sync: true,
-        transitions: [
-            {
-                async leave(data) {
-                    const done = this.async();
+  barba.init({
+    sync: true,
+    transitions: [
+      {
+        async leave(data) {
+          const done = this.async();
 
-                    pageTransition();
-                    await delay(800);
-                    done();
-                },
+          pageTransition();
+          await delay(800);
+          done();
+        },
 
-                async enter(data) {
-                    contentAnimation();
-                },
+        async enter(data) {
+          contentAnimation();
+        },
 
-                async once(data) {
-                    contentAnimation();
-                },
-            },
-        ],
-    });
+        async once(data) {
+          contentAnimation();
+        },
+      },
+    ],
+  });
 });
